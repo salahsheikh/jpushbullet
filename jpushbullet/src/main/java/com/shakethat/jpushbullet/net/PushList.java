670b320310816c9723f242f23d1fb272d9090076
Anthony Snavely <anthony.snavely@gmail.com>
2014-09-18 08:01:28 -0700

    getPushes()
    added a get pushes function

---
+++ jpushbullet/src/main/java/com/shakethat/jpushbullet/net/PushList.java
@@ -0,0 +1,19 @@
+package com.shakethat.jpushbullet.net;
+
+import java.util.List;
+
+/**
+ * Created by Goat on 9/14/2014.
+ */
+public class PushList {
+
+    private List<Push> pushes;
+
+    public List<Push> getPushes() {
+        return pushes;
+    }
+
+    public void setPushes(List<Push> pushes) {
+        this.pushes = pushes;
+    }
+}
